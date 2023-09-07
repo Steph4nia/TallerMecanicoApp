e73b614098e234421c0b6fa5e54b022eadd30b7c
steph4nia <stephanialucerob@gmail.com>
2023-09-07 01:07:42 -0300

    actualizadologin y restablecer contraseña

--- TallerMecanicoApp/src/app/login/login.page.ts
+++ TallerMecanicoApp/src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  
+  formLogin = {
+    usuario: "",
+    password: ""
+  }
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  iniciarSesion() {
+    console.log("usuario: " + this.formLogin.usuario);
+    console.log("password: " + this.formLogin.password);
+
+    // Crear el objeto NavigationExtras con los datos a enviar
+    let datosEnviar: NavigationExtras = {
+      queryParams: { 
+        usuario: this.formLogin.usuario, // Establecer usuario
+        edad: 24 // Ejemplo de otro dato
+      }
+    };
+
+    // Utilizar navigate con los datos a enviar
+    this.router.navigate(['home'], datosEnviar);
+  }
 }
